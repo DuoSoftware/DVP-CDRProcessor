c16c32af34b0bd0c80ff2c9ec3a31567ade37088
dinusha_dck <dinusha@duosoftware.com>
2015-04-24 13:39:13 +0530

    Debug Testing 1

--- app.js
+++ app.js
@@ -1,212 +1,219 @@
-var restify = require('restify');
-var backendHandler = require('./DBBackendHandler.js');
-var stringify = require('stringify');
-var dbModel = require('DVP-DBModels');
-var underscore = require('underscore');
-var config = require('config');
-
-var hostIp = config.Host.Ip;
-var hostPort = config.Host.Port;
-var hostVersion = config.Host.Version;
-
-
-var server = restify.createServer({
-    name: 'DVP-CDRProcessor'
-});
-
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-
-var ProcessBatchCDR = function(cdrList)
+try
 {
-    var emptyArr = [];
+    var restify = require('restify');
+    var backendHandler = require('./DBBackendHandler.js');
+    var stringify = require('stringify');
+    var dbModel = require('DVP-DBModels');
+    var underscore = require('underscore');
+    var config = require('config');
 
-    try
+    var hostIp = config.Host.Ip;
+    var hostPort = config.Host.Port;
+    var hostVersion = config.Host.Version;
+
+
+    var server = restify.createServer({
+        name: 'DVP-CDRProcessor'
+    });
+
+    server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.queryParser());
+    server.use(restify.bodyParser());
+
+    var ProcessBatchCDR = function(cdrList)
     {
-        var arr = underscore.groupBy(cdrList, 'CallUuid');
+        var emptyArr = [];
 
-        return arr;
-    }
-    catch(ex)
-    {
-        return undefined;
-    }
-};
-
-server.get('/DVP/API/' + hostVersion + '/CallCDR/GetCallDetailsByRange/:startTime/:endTime/:companyId/:tenantId', function(req, res, next)
-{
-    try
-    {
-        var startTime = req.params.startTime;
-        var endTime = req.params.endTime;
-        var companyId = req.params.companyId;
-        var tenantId = req.params.tenantId;
-
-        backendHandler.GetCallRelatedLegsInDateRange(startTime, endTime, companyId, tenantId, function(err, legs)
+        try
         {
-            var processedCdr = ProcessBatchCDR(legs);
-            var jsonString = JSON.stringify(processedCdr);
+            var arr = underscore.groupBy(cdrList, 'CallUuid');
 
-            res.end(jsonString);
-        })
-
-    }
-    catch(ex)
-    {
-        res.end('{[}}');
-    }
-
-    return next();
-});
-
-server.get('/DVP/API/' + hostVersion + '/CallCDR/GetCallDetails/:sessionId', function(req, res, next)
-{
-    try
-    {
-        var sessionId = req.params.sessionId;
-
-        backendHandler.GetCallRelatedLegs(sessionId, function(err, legs)
-        {
-            var jsonString = JSON.stringify(legs);
-
-            res.end(jsonString);
-        })
-
-    }
-    catch(ex)
-    {
-        res.end('{[}}');
-    }
-
-    return next();
-});
-
-
-server.post('/DVP/API/' + hostVersion + '/CallCDR/ProcessCDR', function(req,res,next)
-{
-    try
-    {
-        var cdrObj = req.body;
-
-        var rawCDR = JSON.stringify(cdrObj);
-
-        console.log(rawCDR);
-        console.log('\n\n\n\n');
-
-        var varSec = cdrObj['variables'];
-        var callFlowSec = cdrObj['callflow'];
-        var timesSec = callFlowSec['times'];
-        var callerProfileSec = callFlowSec['caller_profile'];
-
-        var uuid = varSec['uuid'];
-        var callUuid = varSec['call_uuid'];
-        var bridgeUuid = varSec['bridge_uuid'];
-        var sipFromUser = callerProfileSec['caller_id_number'];
-        var sipToUser = callerProfileSec['destination_number'];
-        var hangupCause = varSec['hangup_cause'];
-        var direction = varSec['direction'];
-        var switchName = cdrObj['switchname'];
-        var callerContext = callerProfileSec['context'];
-        var answerDate = undefined;
-        var createdDate = undefined;
-        var bridgeDate = undefined;
-        var hangupDate = undefined;
-
-
-        var answeredTimeStamp = timesSec['answered_time'];
-        if(answeredTimeStamp)
-        {
-            var ansTStamp = parseInt(answeredTimeStamp)/1000;
-            answerDate = new Date(ansTStamp);
+            return arr;
         }
-
-        var createdTimeStamp = timesSec['created_time'];
-        if(createdTimeStamp)
+        catch(ex)
         {
-            var createdTStamp = parseInt(createdTimeStamp)/1000;
-            createdDate = new Date(createdTStamp);
+            return undefined;
         }
+    };
 
-        var bridgedTimeStamp = timesSec['bridged_time'];
-        if(bridgedTimeStamp)
+    server.get('/DVP/API/' + hostVersion + '/CallCDR/GetCallDetailsByRange/:startTime/:endTime/:companyId/:tenantId', function(req, res, next)
+    {
+        try
         {
-            var bridgedTStamp = parseInt(bridgedTimeStamp)/1000;
-            bridgeDate = new Date(bridgedTStamp);
-        }
+            var startTime = req.params.startTime;
+            var endTime = req.params.endTime;
+            var companyId = req.params.companyId;
+            var tenantId = req.params.tenantId;
 
-        var hangupTimeStamp = timesSec['hangup_time'];
-        if(hangupTimeStamp)
-        {
-            var hangupTStamp = parseInt(hangupTimeStamp)/1000;
-            hangupDate = new Date(hangupTStamp);
-        }
-
-
-
-        var isAnswered = timesSec['answered_time'] != undefined;
-        var duration = varSec['duration'];
-        var billSec = varSec['billsec'];
-        var holdSec = varSec['hold_accum_seconds'];
-        var progressSec = varSec['progresssec'];
-        var answerSec = varSec['answersec'];
-        var waitSec = varSec['waitsec'];
-        var progressMediaSec = varSec['progress_mediasec'];
-        var flowBillSec = varSec['flow_billsec'];
-
-        var cdr = dbModel.CallCDR.build({
-            Uuid: uuid,
-            CallUuid: callUuid,
-            BridgeUuid: bridgeUuid,
-            SipFromUser: sipFromUser,
-            SipToUser: sipToUser,
-            HangupCause: hangupCause,
-            Direction: direction,
-            SwitchName: switchName,
-            CallerContext: callerContext,
-            IsAnswered: isAnswered,
-            CreatedTime: createdDate,
-            AnsweredTime: answerDate,
-            BridgedTime: bridgeDate,
-            HangupTime: hangupDate,
-            Duration: duration,
-            BillSec: billSec,
-            HoldSec: holdSec,
-            ProgressSec: progressSec,
-            AnswerSec: answerSec,
-            WaitSec: waitSec,
-            ProgressMediaSec: progressMediaSec,
-            FlowBillSec: flowBillSec,
-            ObjClass: 'CDR',
-            ObjType: 'CALL',
-            ObjCategory: undefined,
-            CompanyId: 1,
-            TenantId: 3
-        });
-
-        backendHandler.AddCDRRecord(cdr, function(err, result)
-        {
-            if(err)
+            backendHandler.GetCallRelatedLegsInDateRange(startTime, endTime, companyId, tenantId, function(err, legs)
             {
-                res.end('{}');
-            }
-            else
-            {
-                res.end('{}');
-            }
-        });
+                var processedCdr = ProcessBatchCDR(legs);
+                var jsonString = JSON.stringify(processedCdr);
 
-        //Read App details and push it to the common app event processor
+                res.end(jsonString);
+            })
 
-    }
-    catch(ex)
+        }
+        catch(ex)
+        {
+            res.end('{[}}');
+        }
+
+        return next();
+    });
+
+    server.get('/DVP/API/' + hostVersion + '/CallCDR/GetCallDetails/:sessionId', function(req, res, next)
     {
-        res.end("{}");
-    }
+        try
+        {
+            var sessionId = req.params.sessionId;
 
-    return next();
-});
+            backendHandler.GetCallRelatedLegs(sessionId, function(err, legs)
+            {
+                var jsonString = JSON.stringify(legs);
 
-server.listen(hostPort, hostIp, function () {
-    console.log('%s listening at %s', server.name, server.url);
-});
+                res.end(jsonString);
+            })
+
+        }
+        catch(ex)
+        {
+            res.end('{[}}');
+        }
+
+        return next();
+    });
+
+
+    server.post('/DVP/API/' + hostVersion + '/CallCDR/ProcessCDR', function(req,res,next)
+    {
+        try
+        {
+            var cdrObj = req.body;
+
+            var rawCDR = JSON.stringify(cdrObj);
+
+            console.log(rawCDR);
+            console.log('\n\n\n\n');
+
+            var varSec = cdrObj['variables'];
+            var callFlowSec = cdrObj['callflow'];
+            var timesSec = callFlowSec['times'];
+            var callerProfileSec = callFlowSec['caller_profile'];
+
+            var uuid = varSec['uuid'];
+            var callUuid = varSec['call_uuid'];
+            var bridgeUuid = varSec['bridge_uuid'];
+            var sipFromUser = callerProfileSec['caller_id_number'];
+            var sipToUser = callerProfileSec['destination_number'];
+            var hangupCause = varSec['hangup_cause'];
+            var direction = varSec['direction'];
+            var switchName = cdrObj['switchname'];
+            var callerContext = callerProfileSec['context'];
+            var answerDate = undefined;
+            var createdDate = undefined;
+            var bridgeDate = undefined;
+            var hangupDate = undefined;
+
+
+            var answeredTimeStamp = timesSec['answered_time'];
+            if(answeredTimeStamp)
+            {
+                var ansTStamp = parseInt(answeredTimeStamp)/1000;
+                answerDate = new Date(ansTStamp);
+            }
+
+            var createdTimeStamp = timesSec['created_time'];
+            if(createdTimeStamp)
+            {
+                var createdTStamp = parseInt(createdTimeStamp)/1000;
+                createdDate = new Date(createdTStamp);
+            }
+
+            var bridgedTimeStamp = timesSec['bridged_time'];
+            if(bridgedTimeStamp)
+            {
+                var bridgedTStamp = parseInt(bridgedTimeStamp)/1000;
+                bridgeDate = new Date(bridgedTStamp);
+            }
+
+            var hangupTimeStamp = timesSec['hangup_time'];
+            if(hangupTimeStamp)
+            {
+                var hangupTStamp = parseInt(hangupTimeStamp)/1000;
+                hangupDate = new Date(hangupTStamp);
+            }
+
+
+
+            var isAnswered = timesSec['answered_time'] != undefined;
+            var duration = varSec['duration'];
+            var billSec = varSec['billsec'];
+            var holdSec = varSec['hold_accum_seconds'];
+            var progressSec = varSec['progresssec'];
+            var answerSec = varSec['answersec'];
+            var waitSec = varSec['waitsec'];
+            var progressMediaSec = varSec['progress_mediasec'];
+            var flowBillSec = varSec['flow_billsec'];
+
+            var cdr = dbModel.CallCDR.build({
+                Uuid: uuid,
+                CallUuid: callUuid,
+                BridgeUuid: bridgeUuid,
+                SipFromUser: sipFromUser,
+                SipToUser: sipToUser,
+                HangupCause: hangupCause,
+                Direction: direction,
+                SwitchName: switchName,
+                CallerContext: callerContext,
+                IsAnswered: isAnswered,
+                CreatedTime: createdDate,
+                AnsweredTime: answerDate,
+                BridgedTime: bridgeDate,
+                HangupTime: hangupDate,
+                Duration: duration,
+                BillSec: billSec,
+                HoldSec: holdSec,
+                ProgressSec: progressSec,
+                AnswerSec: answerSec,
+                WaitSec: waitSec,
+                ProgressMediaSec: progressMediaSec,
+                FlowBillSec: flowBillSec,
+                ObjClass: 'CDR',
+                ObjType: 'CALL',
+                ObjCategory: undefined,
+                CompanyId: 1,
+                TenantId: 3
+            });
+
+            backendHandler.AddCDRRecord(cdr, function(err, result)
+            {
+                if(err)
+                {
+                    res.end('{}');
+                }
+                else
+                {
+                    res.end('{}');
+                }
+            });
+
+            //Read App details and push it to the common app event processor
+
+        }
+        catch(ex)
+        {
+            res.end("{}");
+        }
+
+        return next();
+    });
+
+    server.listen(hostPort, hostIp, function () {
+        console.log('%s listening at %s', server.name, server.url);
+    });
+}
+catch(ex)
+{
+    console.log(ex);
+}
