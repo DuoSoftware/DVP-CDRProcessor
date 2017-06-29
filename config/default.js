93ebbb8a5064546d709c92d011caa770da31d1b4
sukitha <sukitha@duosoftware.com>
2017-06-29 12:01:17 +0530

    amqp and ioredis library added clustering with failover support

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   "DB": {
     "Type":"postgres",
     "User":"duo",
@@ -7,19 +8,35 @@ module.exports = {
     "Host":"104.236.231.11",
     "Database":"duo"
   },
-  "Security":
-  {
-    "ip" : "45.55.142.207",
-    "port": 6389,
-    "password": "DuoS123"
-  },
 
   "Redis":
   {
+    "mode":"sentinel",//instance, cluster, sentinel
     "ip": "45.55.142.207",
     "port": 6389,
-    "password":"DuoS123"
+    "user": "duo",
+    "password": "DuoS123",
+    "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
 
+  },
+
+  "Security":
+  {
+
+    "ip" : "45.55.142.207",
+    "port": 6389,
+    "user": "duo",
+    "password": "DuoS123",
+    "mode":"sentinel",//instance, cluster, sentinel
+    "sentinels":{
+      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "port":16389,
+      "name":"redis-cluster"
+    }
   },
 
   "RabbitMQ":
@@ -27,7 +44,8 @@ module.exports = {
     "ip": "45.55.142.207",
     "port": 5672,
     "user": "admin",
-    "password": "admin"
+    "password": "admin",
+    "vhost":'/'
   },
 
   "Mongo":
