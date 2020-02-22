6545b6acc97dce67e70259fa9ec79419109263fa
sukitha jayasingha <sukithaj@gmail.com>
2020-02-22 17:15:25 +0530

    amqp log

--- MailSender.js
+++ MailSender.js
@@ -2,19 +2,17 @@
  * Created by dinusha on 11/23/2016.
  */
 
-var format = require('stringformat');
-var config = require('config');
-var amqp = require('amqp');
-var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
+var format = require("stringformat");
+var config = require("config");
+var amqp = require("amqp");
+var logger = require("dvp-common/LogHandler/CommonLogHandler.js").logger;
 
-
-
-if(config.RabbitMQ.ip) {
-    config.RabbitMQ.ip = config.RabbitMQ.ip.split(",");
+if (config.RabbitMQ.ip) {
+  config.RabbitMQ.ip = config.RabbitMQ.ip.split(",");
 }
 
-
-var queueConnection = amqp.createConnection({
+var queueConnection = amqp.createConnection(
+  {
     //url: queueHost,
     host: config.RabbitMQ.ip,
     port: config.RabbitMQ.port,
@@ -22,36 +20,38 @@ var queueConnection = amqp.createConnection({
     password: config.RabbitMQ.password,
     vhost: config.RabbitMQ.vhost,
     noDelay: true,
-    heartbeat:10
-}, {
+    heartbeat: 10
+  },
+  {
     reconnect: true,
-    reconnectBackoffStrategy: 'linear',
+    reconnectBackoffStrategy: "linear",
     reconnectExponentialLimit: 120000,
     reconnectBackoffTime: 1000
+  }
+);
+
+queueConnection.on("ready", function() {
+  logger.info("Conection with the queue is OK");
 });
 
-queueConnection.on('ready', function () {
-
-    logger.info("Conection with the queue is OK");
+queueConnection.on("error", function(error) {
+  logger.info("There is an error" + error);
 });
 
-queueConnection.on('error', function (error) {
-
-    logger.info("There is an error" + error);
+queueConnection.on("heartbeat", function() {
+  logger.debug("RabbitMQ HeartBeat");
 });
 
-module.exports.PublishToQueue = function(messageType,sendObj ) {
+module.exports.PublishToQueue = function(messageType, sendObj) {
+  logger.info("Email Send : " + JSON.stringify(sendObj));
 
-    logger.info("Email Send : " + JSON.stringify(sendObj));
-
-    try {
-        if (sendObj) {
-            queueConnection.publish(messageType, sendObj, {
-                contentType: 'application/json'
-            });
-        }
-    } catch (exp) {
-
-        console.log(exp);
+  try {
+    if (sendObj) {
+      queueConnection.publish(messageType, sendObj, {
+        contentType: "application/json"
+      });
     }
-}
+  } catch (exp) {
+    console.log(exp);
+  }
+};
